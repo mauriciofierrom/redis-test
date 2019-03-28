9e2c5fbdc6f86b32440d1af7f6947afc8a56b166
Mauricio Fierro <mauriciofierrom@gmail.com>
2019-03-28 06:29:42 -0500

    Basic string lookup

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,9 @@
 
 module Main where
 
+import Prelude hiding (lookup)
 import Database.Redis.IO
+import qualified Data.ByteString.Lazy as B
 
 import Lib
 import Data.Bool (bool)
@@ -12,9 +14,14 @@ main = do
   pool <- getPool
   res <- runRedis pool (commands s)
   putStrLn (line res)
+  res1 <- runRedis pool (commands l)
+  print res1
     where
       s :: Redis IO Bool
       s = add "key" "value"
 
+      l :: Redis IO B.ByteString
+      l = lookup "key"
+
       line :: Bool -> String
-      line = bool "error" "Yeah!"
+      line = bool "error" "Saved!"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Lib where
 
 import Database.Redis.IO
 import System.Logger
+import qualified Data.ByteString.Lazy as B
 
 add :: Key -> Field -> Redis IO Bool
 add k f = set k f nx
+
+lookup :: Key -> Redis IO B.ByteString
+lookup k =
+  get k >>= \case
+    Just s -> return s
+    Nothing -> error "That shit sucks"
 
 getLogger :: IO Logger
 getLogger = create StdOut
